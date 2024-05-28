208dda363a76d768db9293d70ddbe99863cbd25a
icantthinkofausernamesothiswilldo <161003294+icantthinkofausernamesothiswilldo@users.noreply.github.com>
2024-05-28 15:29:51 +0300

    Update CelestianiumModItems.java

--- src/main/java/com/celestianiummoddev/celestianium/CelestianiumModItems.java
+++ src/main/java/com/celestianiummoddev/celestianium/CelestianiumModItems.java
@@ -5,7 +5,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class CelestianiumModItems {
   public static Item CELESTIANIUM_INGOT = new CelestianiumIngot();
 
-  {
+  public static void init() {
     GameRegistry.registerItem(CELESTIANIUM_INGOT,"item.celestianium_ingot.name");
   }
 }
