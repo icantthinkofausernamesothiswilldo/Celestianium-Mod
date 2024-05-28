186255dcb052ec93adcee8d829cd12b719ed3064
icantthinkofausernamesothiswilldo <161003294+icantthinkofausernamesothiswilldo@users.noreply.github.com>
2024-05-28 15:31:06 +0300

    Update CommonProxy.java

--- src/main/java/com/celestianiummoddev/celestianium/CommonProxy.java
+++ src/main/java/com/celestianiummoddev/celestianium/CommonProxy.java
@@ -8,9 +8,7 @@ public class CommonProxy {
     // etc, and register them with the GameRegistry."
     public void preInit(FMLPreInitializationEvent event) 	{
         Config.syncronizeConfiguration(event.getSuggestedConfigurationFile());
-
-        MyMod.info(Config.greeting);
-        MyMod.info("I am " + Tags.MODNAME + " at version " + Tags.VERSION + " and group name " + Tags.GROUPNAME);
+        CelestianiumModItems.init();
     }
 
     // load "Do your mod setup. Build whatever data structures you care about. Register recipes."
